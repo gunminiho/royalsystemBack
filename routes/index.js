9ff4a8d2b44728cd7162bad531cb59d6200ec847
gunminiho <gunminiho@gmail.com>
2024-05-07 23:08:46 -0500

    working server with routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ const { Router } = require('express');
 const userRoutes = require('./userRoutes');
 const router = Router();
 
-
 console.log("index.js router");
 router.use(userRoutes);
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,15 +5,6 @@ const routes = require("./routes/index");
 
 const { PORT } = process.env || 3000;
 
-// Configurar middleware
-//server.use(express.json());
-//server.use(express.urlencoded({ extended: true }));
-
-/*router.use(express.json());
-router.use(cors());
-router.use(bodyParser.json());
-router.use(cookieParser());
-router.use(morgan("dev"));*/
 server.use("/", routes);
 
 server.listen(PORT, () => {
